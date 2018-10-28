bf521b5589d489323a1fa0f265bc667fbb43e17d
ciscohuangchuyu <18312755572@139.com>
2018-10-28 16:47:00 +0800

    第一次提交

---
+++ src/main/java/com/example/mybatisdemo/MybatisdemoApplication.java
@@ -0,0 +1,48 @@
+package com.example.mybatisdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@Slf4j
+@MapperScan("com.example.mybatisdemo.dao")
+@SpringBootApplication
+public class MybatisdemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MybatisdemoApplication.class, args);
+        log.info(notBug());
+    }
+    private static String notBug (){
+        String str = "\n" +
+                "/**\n" +
+                "                   _oo0oo_\n" +
+                "                  o8888888o\n" +
+                "                  88\" . \"88\n" +
+                "                  (| -_- |)\n" +
+                "                  0\\  =  /0\n" +
+                "                ___/`---'\\___\n" +
+                "              .' --|     |-- '.\n" +
+                "             / --|||  :  |||-- \\\n" +
+                "            / _||||| -:- |||||- \\\n" +
+                "           |   | \\--  -  --/ |   |\n" +
+                "           | \\_|  ''\\---/''  |_/ |\n" +
+                "           \\  .-\\__  '-'  ___/-. /\n" +
+                "         ___'. .'  /--.--\\  `. .'___\n" +
+                "      .\"\" '<  `.___\\_<|>_/___.' >' \"\".\n" +
+                "     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n" +
+                "     \\  \\ `_.   \\_ __\\ /__ _/   .-` /  /\n" +
+                " =====`-.____`.___ \\_____/___.-`___.-'=====\n" +
+                "                   `=---=`\n" +
+                "\n" +
+                " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                "\n" +
+                "             佛祖保佑     永无BUG\n" +
+                " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+                " \n" +
+                " **/";
+        return str;
+    }
+
+}
---
+++ src/main/java/com/example/mybatisdemo/persistence/auto/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.example.mybatisdemo.persistence.auto.mapper;
+
+import com.example.mybatisdemo.persistence.auto.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
